f8e974b7b25fa7bb1f7f0b2e60d83dfe9399d981
Andy <onsubmit@outlook.com>
2023-10-15 22:26:51 -0700

    Add some config data validation

--- src/config.ts
+++ src/config.ts
@@ -11,17 +11,17 @@ export default class Config {
   private _frameDelay: number;
 
   constructor(params: { [key: string]: string }) {
-    this._canvasWidth = parseInt(params.canvasWidth, 10) || 800;
-    this._canvasHeight = parseInt(params.canvasHeight, 10) || 800;
+    this._canvasWidth = Math.max(800, parseInt(params.canvasWidth, 10) || 800);
+    this._canvasHeight = Math.max(800, parseInt(params.canvasHeight, 10) || 800);
 
-    this._rows = parseInt(params.rows) || 10;
-    this._columns = parseInt(params.columns) || 10;
-    this._cellWidth = Math.floor(this._canvasWidth / this._columns);
-    this._cellHeight = Math.floor(this._canvasHeight / this._rows);
+    this._rows = Math.max(10, parseInt(params.rows) || 10);
+    this._columns = Math.max(10, parseInt(params.columns) || 10);
+    this._cellWidth = Math.max(1, Math.floor(this._canvasWidth / this._columns));
+    this._cellHeight = Math.max(1, Math.floor(this._canvasHeight / this._rows));
     this._cellColor = params.cellColor || '#ffffff';
     this._cellBackgroundColor = params.cellBackgroundColor || '#242424';
-    this._maxStackSize = parseInt(params.maxStackSize) || 4;
-    this._frameDelay = parseInt(params.frameDelay) || 0;
+    this._maxStackSize = Math.max(4, Math.min(255, parseInt(params.maxStackSize) || 4));
+    this._frameDelay = Math.max(0, parseInt(params.frameDelay) || 0);
 
     if (this._cellColor && !this._cellColor.startsWith('#')) {
       this._cellColor = `#${this._cellColor}`;
